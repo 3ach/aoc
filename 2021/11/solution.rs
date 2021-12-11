fa90725aa7d1e452836613d8eff1c370f2bfcadc
Zach Zundel <me@zachzundel.com>
2021-12-10 22:19:40 -0800

    2021-10 cleanup

--- 2021/11/solution.rs
+++ 2021/11/solution.rs
@@ -4,136 +4,75 @@ use std::cmp;
 use std::collections::HashSet;
 use std::convert::TryInto;
 
-struct Octopus<'a> {
-    energy: u8,
-    neighbors: [Option<&'a Octopus<'a>>; 8],
-    last_flash: usize,
-}
+fn step(octopodes: &mut Vec<Vec<u8>>) -> usize {
+    let mut flashed: HashSet<(isize, isize)> = HashSet::new();
+    let mut energized: Vec<(isize, isize)> = vec![];
 
-fn part1(octopodes: &Vec<Vec<u8>>) -> u32 {
-    let mut flashes = 0;
-    let mut octopodes = octopodes.clone();
+    for row in 0..octopodes.len() {
+        for col in 0..octopodes.len() {
+            energized.push((row as isize, col as isize));
+        }
+    }
 
-    for step in 0..100 {
-        let mut flashed: HashSet<(isize, isize)> = HashSet::new();
-        let mut energized: Vec<(isize, isize)> = vec![];
-
-        for row in 0..octopodes.len() {
-            for col in 0..octopodes.len() {
-                energized.push((row as isize, col as isize));
-            }
+    while energized.len() > 0 {
+        let (row, col) = energized.remove(0);
+        if row < 0 || row >= octopodes.len() as isize || col < 0 || col >= octopodes[0].len() as isize {
+            continue;
+        }
+        
+        if flashed.contains(&(row, col)) {
+            continue;
         }
 
-        while energized.len() > 0 {
-            let (row, col) = energized.remove(0);
-            if row < 0 || row >= octopodes.len() as isize || col < 0 || col >= octopodes[0].len() as isize {
-                continue;
-            }
-            
-            if flashed.contains(&(row, col)) {
-                continue;
-            }
+        octopodes[row as usize][col as usize] += 1;
 
-            octopodes[row as usize][col as usize] += 1;
+        if octopodes[row as usize][col as usize] > 9 {
+            flashed.insert((row, col));
 
-            if octopodes[row as usize][col as usize] > 9 {
-                flashed.insert((row, col));
-
-                for rdiff in -1..=1 {
-                    for cdiff in -1..=1 {
-                        if rdiff == 0 && cdiff == 0 {
-                            continue
-                        }
-
-                        if flashed.contains(&(row + rdiff, col + cdiff)) {
-                            continue
-                        }
-
-                        energized.push((row + rdiff, col + cdiff));
+            for rdiff in -1..=1 {
+                for cdiff in -1..=1 {
+                    if rdiff == 0 && cdiff == 0 {
+                        continue
                     }
+
+                    if flashed.contains(&(row + rdiff, col + cdiff)) {
+                        continue
+                    }
+
+                    energized.push((row + rdiff, col + cdiff));
                 }
             }
         }
+    }
 
-        flashes += flashed.len() as u32;
+    for (row, col) in &flashed {
+        octopodes[*row as usize][*col as usize] = 0;
+    }
 
-        for (row, col) in flashed {
-            octopodes[row as usize][col as usize] = 0;
-        }
+    return flashed.len();
+}
+
+fn part1(octopodes: &Vec<Vec<u8>>) -> usize {
+    let mut flashes = 0;
+    let mut octopodes = octopodes.clone();
+
+    for _ in 0..100 {
+        flashes += step(&mut octopodes);
     }
 
     return flashes;
 }
 
-fn print_energies(octopodes: &Vec<Vec<u8>>) {
-    for r in 0..octopodes.len() {
-        for c in 0..octopodes[0].len() {
-            if octopodes[r][c] > 0 {
-                print!("{:x}", octopodes[r][c]);
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
-}
-
 fn part2(octopodes: &Vec<Vec<u8>>) -> u32 {
-    let target = octopodes.len() * octopodes[0].len();
     let mut octopodes = octopodes.clone();
-    let mut step = 0;
+    let target = octopodes.len() * octopodes[0].len();
+    let mut steps = 1;
 
-    loop {
-        step += 1;
-        let mut flashed: HashSet<(isize, isize)> = HashSet::new();
-        let mut energized: Vec<(isize, isize)> = vec![];
-
-        for row in 0..octopodes.len() {
-            for col in 0..octopodes.len() {
-                energized.push((row as isize, col as isize));
-            }
-        }
-
-        while energized.len() > 0 {
-            let (row, col) = energized.remove(0);
-            if row < 0 || row >= octopodes.len() as isize || col < 0 || col >= octopodes[0].len() as isize {
-                continue;
-            }
-            
-            if flashed.contains(&(row, col)) {
-                continue;
-            }
-
-            octopodes[row as usize][col as usize] += 1;
-
-            if octopodes[row as usize][col as usize] > 9 {
-                flashed.insert((row, col));
-
-                for rdiff in -1..=1 {
-                    for cdiff in -1..=1 {
-                        if rdiff == 0 && cdiff == 0 {
-                            continue
-                        }
-
-                        if flashed.contains(&(row + rdiff, col + cdiff)) {
-                            continue
-                        }
-
-                        energized.push((row + rdiff, col + cdiff));
-                    }
-                }
-            }
-        }
-
-
-        if flashed.len() == target {
-            return step
-        }
-
-        for (row, col) in flashed {
-            octopodes[row as usize][col as usize] = 0;
-        }
+    while step(&mut octopodes) != target {
+        steps += 1;
     }
+
+    return steps;
 }
 
 fn main() -> io::Result<()> {
