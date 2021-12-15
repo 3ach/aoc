baec544da973ccc6c7bf71efddbd25d5b52a3eff
Zach Zundel <me@zachzundel.com>
2021-12-14 23:42:11 -0800

    2021-15 speedup

--- 2021/15/solution.rs
+++ 2021/15/solution.rs
@@ -1,79 +1,57 @@
 use std::io::BufRead;
 use std::io;
-use std::cmp::Ordering;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::convert::TryInto;
-
-#[derive(Debug,Clone,Copy,Eq,PartialEq)]
-struct RiskLevel {
-    coord: (usize, usize),
-    risk_to_here: usize,
-}
-
-impl Ord for RiskLevel {
-    fn cmp(&self, other: &RiskLevel) -> Ordering {
-        return other.risk_to_here.cmp(&self.risk_to_here);
-    }
-}
-
-impl PartialOrd for RiskLevel {
-    fn partial_cmp(&self, other: &RiskLevel) -> Option<Ordering> {
-        return Some(self.cmp(other));
-    }
-}
-
 
 fn part1(chiton: &Vec<Vec<u8>>) -> usize {
     let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    let mut next: Vec<RiskLevel> = vec![];
+    let mut next: HashMap<(usize, usize), usize> = HashMap::new();
 
-    next.push(RiskLevel{coord: (0, 0), risk_to_here: 0});
+    next.insert((0, 0), 0);
 
     while !next.is_empty() {
-        let risk = *next.iter().min_by_key(|risk| risk.risk_to_here).unwrap();
+        let (coord, risk) = next.iter().min_by_key(|(_, risk)| *risk).unwrap();
+        let (coord, risk) = (*coord, *risk);
 
-        if risk.coord == (chiton.len() - 1, chiton[0].len() - 1) {
-            return risk.risk_to_here;
+        if coord == (chiton.len() - 1, chiton[0].len() - 1) {
+            return risk;
         }
 
-        visited.insert(risk.coord);
-        next = next.iter().copied().filter(|n| n.coord != risk.coord).collect();
+        visited.insert(coord);
+        next.remove(&coord);
+
         let mut neighbors: Vec<(usize, usize)> = vec![];
 
-        if risk.coord.0 > 0 {
-            neighbors.push((risk.coord.0 - 1, risk.coord.1));
+        if coord.0 > 0 {
+            neighbors.push((coord.0 - 1, coord.1));
         }
 
-        if risk.coord.1 > 0 {
-            neighbors.push((risk.coord.0, risk.coord.1 - 1));
+        if coord.1 > 0 {
+            neighbors.push((coord.0, coord.1 - 1));
         }
 
-        if risk.coord.0 < chiton.len() - 1 {
-            neighbors.push((risk.coord.0 + 1, risk.coord.1));
+        if coord.0 < chiton.len() - 1 {
+            neighbors.push((coord.0 + 1, coord.1));
         }
 
-        if risk.coord.1 < chiton[0].len() - 1 {
-            neighbors.push((risk.coord.0, risk.coord.1 + 1));
+        if coord.1 < chiton[0].len() - 1 {
+            neighbors.push((coord.0, coord.1 + 1));
         }
 
         for neighbor in neighbors {
             if !visited.contains(&neighbor) {
-                let risk_to_neighbor = risk.risk_to_here + chiton[neighbor.0][neighbor.1] as usize;
-                next.push(RiskLevel{coord: neighbor, risk_to_here: risk_to_neighbor})
+                let risk_to_neighbor = risk + chiton[neighbor.0][neighbor.1] as usize;
+                let entry = next.entry(neighbor).or_insert(risk_to_neighbor + 1);
+    
+                if *entry > risk_to_neighbor {
+                    *entry = risk_to_neighbor
+                }
             }
         }
     }
 
     panic!();
-}
-
-fn print_reading(reading: &Vec<Vec<u8>>) {
-    for row in 0..reading.len() {
-        for col in 0..reading[row].len() {
-            print!("{}", reading[row][col]);
-        }
-        println!("");
-    }
 }
 
 fn part2(chiton: &Vec<Vec<u8>>) -> usize {
