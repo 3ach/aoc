6f7857137bcb99119ec48a767f62630107822fdc
Zach Zundel <me@zachzundel.com>
2022-12-21 14:13:16 -0500

    2022-21 pt2

--- 2022/21/solution.rs
+++ 2022/21/solution.rs
@@ -1,15 +1,15 @@
-use std::io::BufRead;
-use std::io;
 use std::collections::HashMap;
+use std::io;
+use std::io::BufRead;
 
 #[derive(Debug, Clone)]
 enum Op {
+    Eq,
     Add,
     Subtract,
-    Divide, 
-    Multiply
+    Divide,
+    Multiply,
 }
-
 
 #[derive(Debug, Clone)]
 enum Job {
@@ -17,24 +17,25 @@ enum Job {
     Number(i64),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Monkey {
     name: String,
-    job: Job
+    job: Job,
 }
 
 type TInput = Vec<Monkey>;
 
-fn dependency_tree
-
-fn part1(input: &TInput) -> i64 {
-    let mut resolved: HashMap<_, i64> = input.iter()
+fn resolve(input: &TInput, which: String) -> Option<i64> {
+    let mut resolved: HashMap<_, i64> = input
+        .iter()
         .filter_map(|monkey| match &monkey.job {
             Job::Number(num) => Some((monkey.name.clone(), *num)),
             _ => None,
-        }).collect();
+        })
+        .collect();
 
-    while !resolved.contains_key("root") {
+    while !resolved.contains_key(&which) {
+        let resolved_count = resolved.len();
         for monkey in input {
             if resolved.contains_key(&monkey.name) {
                 continue;
@@ -47,24 +48,76 @@ fn part1(input: &TInput) -> i64 {
                         Op::Subtract => resolved.insert(monkey.name.clone(), lhs - rhs),
                         Op::Multiply => resolved.insert(monkey.name.clone(), lhs * rhs),
                         Op::Divide => resolved.insert(monkey.name.clone(), lhs / rhs),
+                        _ => panic!(),
                     };
                 }
             }
         }
+
+        if resolved_count == resolved.len() {
+            return None;
+        }
     }
 
-    *resolved.get("root").unwrap()
+    Some(resolved[&which])
+}
+
+fn part1(input: &TInput) -> i64 {
+    resolve(input, "root".to_string()).unwrap()
 }
 
 fn part2(input: &TInput) -> i64 {
-    0
+    let input: Vec<Monkey> = input
+        .iter()
+        .cloned()
+        .filter(|m| m.name != "humn")
+        .map(|mut m| {
+            if m.name == "root" {
+		if let Job::Math(lhs, ref mut op, rhs) = &mut m.job {
+			*op = Op::Eq;
+		}
+            }
+            m
+        })
+        .collect();
+
+    let mut current = "root".to_string();
+    let mut carry = 0;
+
+    loop {
+	if current == "humn" { return carry; }
+        let monkey = input.iter().filter(|m| m.name == current).next().unwrap();
+        if let Job::Math(lhs, op, rhs) = &monkey.job {
+            let left = resolve(&input, lhs.to_string());
+            let right = resolve(&input, rhs.to_string());
+
+	    let (unresolved, resolved) = match (left, right) {
+		(None, Some(value)) => (lhs, value),
+		(Some(value), None) => (rhs, value),
+		_ => panic!(),
+		};
+
+	    carry = match op  {
+		Op::Eq => resolved,
+		Op::Add => carry - resolved,
+		Op::Subtract => carry + resolved,
+		Op::Multiply => carry / resolved,
+		Op::Divide => carry * resolved,
+	    };
+
+            current = unresolved.to_string();
+        } else {
+            panic!();
+        }
+    }
 }
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let reader = stdin.lock();
 
-    let input: TInput = reader.lines()
+    let input: TInput = reader
+        .lines()
         .map(|line| line.expect("Couldn't read stdin"))
         .map(|line| {
             let (name, job) = line.split_once(": ").unwrap();
@@ -73,24 +126,30 @@ fn main() -> io::Result<()> {
                 let parts = job.split(" ").collect::<Vec<_>>();
                 let op = match parts[1] {
                     "+" => Op::Add,
-                    "-" => Op::Subtract, 
+                    "-" => Op::Subtract,
                     "*" => Op::Multiply,
                     "/" => Op::Divide,
                     _ => panic!("Unknown op {}", parts[1]),
                 };
 
-                Monkey { name: name.to_string(), job: Job::Math(parts[0].to_string(), op, parts[2].to_string()) }
+                Monkey {
+                    name: name.to_string(),
+                    job: Job::Math(parts[0].to_string(), op, parts[2].to_string()),
+                }
             } else {
-                Monkey { name: name.to_string(), job: Job::Number(job.parse().unwrap()) }
+                Monkey {
+                    name: name.to_string(),
+                    job: Job::Number(job.parse().unwrap()),
+                }
             }
         })
         .collect();
 
-	let answer1 = part1(&input);
-	let answer2 = part2(&input);
+    let answer1 = part1(&input);
+    let answer2 = part2(&input);
 
-	println!("Answer 1: {}", answer1);
-	println!("Answer 2: {}", answer2);
+    println!("Answer 1: {}", answer1);
+    println!("Answer 2: {}", answer2);
 
     Ok(())
 }
