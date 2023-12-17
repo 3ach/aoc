a70c83699ad9b74f324c9115ccf5a825658c2194
Zach Zundel <me@zachzundel.com>
2023-12-17 14:42:25 -0800

    template

---
+++ main.rs
@@ -0,0 +1,18 @@
+use adventage::{part1demo, part2demo, day};
+
+day!(YEAR, DAY);
+
+fn parse(input: &str) -> Vec<String> {
+    input
+        .lines()
+        .map(|l| String::from(l))
+        .collect()
+}
+
+fn part1(_map: &Vec<String>) -> u32 {
+    0
+}
+
+fn part2(_map: &Vec<String>) -> u32 {
+    0
+}
